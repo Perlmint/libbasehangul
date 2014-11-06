d9950a683ff8cf495216a08a2f3fc836430bfe37
Gyusun Yeom <omniavinco@gmail.com>
2014-11-07 02:00:13 +0900

    Support Encoding - UCS2 / UTF8

--- include/basehangul.h
+++ include/basehangul.h
@@ -16,33 +16,41 @@ extern "C" {
 #include "type.h"
 #include <string.h>
 
+typedef enum
+{
+    UCS2,
+    UTF8
+} Encoding;
+    
 /**
  * \brief Calculate size of data when encoded
  *
  * \param[in] inputSize Size of data to encode
  * \return              Size of encoded data
  */
-size_t BaseHangulEncodeLength(size_t inputSize);
+size_t BaseHangulEncodeLength(size_t inputSize, Encoding encoding);
 
 /**
  * \brief Encode data using basehangul method
  *
- * \param[out] encoded Array for encoded data
- * \param[in]  input   Data to encode
- * \param[in]  len     Length of \ref input
- * \return             Length of \ref encoded
+ * \param[out] encoded     Array for encoded data
+ * \param[in]  input       Data to encode
+ * \param[in]  len         Length of \ref input
+ * \param[in]  encoding    Character Encoding Type
+ * \return                 Length of \ref encoded
  */
-size_t BaseHangulEncode(unsigned char *encoded, const unsigned char *input, size_t len);
+size_t BaseHangulEncode(unsigned char *encoded, const unsigned char *input, size_t len, Encoding encoding);
 
 /**
  * \brief Decode basehangul encoded data
  *
  * \param[out] decoded
- * \param[in]  encoded Basehangul encoded data
- * \param[in]  len     Length of \ref encoded
- * \return             Length of \ref decoded
+ * \param[in]  encoded    Basehangul encoded data
+ * \param[in]  len        Length of \ref encoded
+ * \param[in]  encoding   Encoding of \ref encoded
+ * \return                Length of \ref decoded
  */
-size_t BaseHangulDecode(unsigned char *decoded, const unsigned char *encoded, size_t len);
+size_t BaseHangulDecode(unsigned char *decoded, const unsigned char *encoded, size_t len, Encoding encoding);
     
 const unsigned char *BaseHangulEncodeBlock(uint16_t *encoded, const unsigned char *input, size_t len);
     
--- src/basehangul.c
+++ src/basehangul.c
@@ -9,30 +9,57 @@
 #include "basehangul.h"
 #include "digit_set.h"
 
-size_t BaseHangulEncodeLength(size_t inputSize)
+size_t BaseHangulEncodeLength(size_t inputSize, Encoding encoding)
 {
-    return (inputSize / 5 * 4) + ((inputSize % 5) == 0 ? 0 : 4);
+    uint8_t char_size = 2;
+    switch (encoding) {
+        case UCS2:
+            char_size = 2;
+            break;
+        case UTF8:
+            char_size = 3;
+            break;
+    }
+    return ((inputSize / 5 * 4) + ((inputSize % 5) == 0 ? 0 : 4)) * char_size;
 }
 
-size_t BaseHangulEncode(unsigned char *encoded, const unsigned char *input, size_t len)
+void UCS2toUTF8(unsigned char *output, uint16_t *input)
+{
+    for (uint8_t i = 0; i < 4; ++i)
+    {
+        output[i * 3 + 0] = 0xE0 | ((input[i] & 0xF000) >> 12);
+        output[i * 3 + 1] = 0x80 | ((input[i] & 0x0FC0) >> 6);
+        output[i * 3 + 2] = 0x80 | ((input[i] & 0x003F));
+    }
+}
+
+size_t BaseHangulEncode(unsigned char *encoded, const unsigned char *input, size_t len, Encoding encoding)
 {
     uint16_t buffer[4] = {0};
     
     size_t written = 0;
-    for (size_t processed = 0, end = len - 4; processed < end; processed += 5, written += 4)
+    for (size_t processed = 0, end = len; processed < end; processed += 5, written += 4)
     {
         memset(buffer, 0, sizeof(buffer));
         
         input = BaseHangulEncodeBlock(buffer, input, len - processed);
         
-        memcpy(encoded, buffer, sizeof(buffer));
-        encoded += sizeof(buffer);
+        switch (encoding) {
+            case UCS2:
+                memcpy(encoded, buffer, sizeof(buffer));
+                encoded += sizeof(buffer);
+                break;
+            case UTF8:
+                UCS2toUTF8(encoded, buffer);
+                encoded += 4 * 3;
+                break;
+        }
     }
     
     return written;
 }
 
-size_t BaseHangulDecode(unsigned char *decoded, const unsigned char *encoded, size_t len)
+size_t BaseHangulDecode(unsigned char *decoded, const unsigned char *encoded, size_t len, Encoding encoding)
 {
     // Not implemented yet
     return 0;
@@ -45,40 +72,24 @@ const unsigned char *BaseHangulEncodeBlock(uint16_t *encoded, const unsigned cha
         return input;
     }
     
+    unsigned char block[5] = {0};
+    memcpy(block, input, len);
+    
+    encoded[0] = digit_set[(input[0] << 2) | (block[1] >> 6)];
+    encoded[1] = digit_set[((input[1] & 0x3F) << 4) | (block[2] >> 4)];
+    encoded[2] = digit_set[((input[2] & 0x0F) << 6) | (block[3] >> 2)];
+    encoded[3] = digit_set[((input[3] & 0x03) << 8) | (block[4])];
+    
     if (len == 4)
     {
-        encoded[0] = digit_set[(input[0] << 2) | (input[1] >> 6)];
-        encoded[1] = digit_set[((input[1] & 0x3F) << 4) | (input[2] >> 4)];
-        encoded[2] = digit_set[((input[2] & 0x0F) << 6) | (input[3] >> 2)];
-        encoded[3] = digit_set[1024 + ((input[3] & 0x03) >> 8)];
+        encoded[3] = digit_set[1024 + ((block[3] & 0x03) >> 8)];
     }
-    else if (len == 3)
+    else if (len <= 3)
     {
-        encoded[0] = digit_set[(input[0] << 2) | (input[1] >> 6)];
-        encoded[1] = digit_set[((input[1] & 0x3F) << 4) | (input[2] >> 4)];
-        encoded[2] = digit_set[((input[2] & 0x0F) << 6)];
-        encoded[3] = digit_set[1028];
-    }
-    else if (len == 2)
-    {
-        encoded[0] = digit_set[(input[0] << 2) | (input[1] >> 6)];
-        encoded[1] = digit_set[((input[1] & 0x3F) << 4)];
-        encoded[2] = digit_set[1028];
-        encoded[3] = digit_set[1028];
-    }
-    else if (len == 1)
-    {
-        encoded[0] = digit_set[input[0] << 2];
-        encoded[1] = digit_set[1028];
-        encoded[2] = digit_set[1028];
-        encoded[3] = digit_set[1028];
-    }
-    else
-    {
-        encoded[0] = digit_set[(input[0] << 2) | (input[1] >> 6)];
-        encoded[1] = digit_set[((input[1] & 0x3F) << 4) | (input[2] >> 4)];
-        encoded[2] = digit_set[((input[2] & 0x0F) << 6) | (input[3] >> 2)];
-        encoded[3] = digit_set[((input[3] & 0x03) << 8) | (input[4])];
+        for (uint8_t i = 3, to = len; i >= to ; --i)
+        {
+            encoded[i] = digit_set[1028];
+        }
     }
     
     return input;
