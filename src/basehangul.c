f820f1a4e43c6f0d7b06158a3d839eb2ce7c4420
Gyusun Yeom <omniavinco@gmail.com>
2015-12-11 03:10:34 +0900

    Remove trailing whitespaces

--- src/basehangul.c
+++ src/basehangul.c
@@ -55,7 +55,7 @@ void UCS2toUTF8(unsigned char *output, const uint16_t *input)
 void UTF8toUCS2(uint16_t *output, const unsigned char *input, uint8_t len)
 {
     len = min(len, 4);
-    
+
     for (uint8_t i = 0; i < len; ++i)
     {
         output[i] =
@@ -68,14 +68,14 @@ void UTF8toUCS2(uint16_t *output, const unsigned char *input, uint8_t len)
 size_t BaseHangulEncode(unsigned char *encoded, const unsigned char *input, size_t len, Encoding encoding)
 {
     uint16_t buffer[4] = {0};
-    
+
     size_t written = 0;
     for (size_t processed = 0, end = len; processed < end; processed += 5, written += 4)
     {
         memset(buffer, 0, sizeof(buffer));
-        
+
         input = BaseHangulEncodeBlock(buffer, input, len - processed);
-        
+
         switch (encoding) {
             case UCS2:
                 memcpy(encoded, buffer, sizeof(buffer));
@@ -87,19 +87,19 @@ size_t BaseHangulEncode(unsigned char *encoded, const unsigned char *input, size
                 break;
         }
     }
-    
+
     return written;
 }
 
 size_t BaseHangulDecode(unsigned char *decoded, const unsigned char *encoded, size_t len, Encoding encoding)
 {
     uint16_t inputbuffer[4] = {0};
-    
+
     size_t written = 0;
     for (size_t processed = 0, end = len; processed < end;)
     {
         memset(inputbuffer, 0, sizeof(inputbuffer));
-        
+
         uint8_t blockSize = 0;
         switch (encoding) {
             case UCS2:
@@ -115,12 +115,12 @@ size_t BaseHangulDecode(unsigned char *decoded, const unsigned char *encoded, si
                 written += blockSize / 15 * 4;
                 break;
         }
-        
+
         BaseHangulDecodeBlock(decoded, inputbuffer, len - processed);
-        
+
         processed += blockSize;
     }
-    
+
     return written;
 }
 
@@ -130,15 +130,15 @@ const unsigned char *BaseHangulEncodeBlock(uint16_t *encoded, const unsigned cha
     {
         return input;
     }
-    
+
     unsigned char block[5] = {0};
     memcpy(block, input, len);
-    
+
     encoded[0] = digit_set[(block[0] << 2) | (block[1] >> 6)];
     encoded[1] = digit_set[((block[1] & 0x3F) << 4) | (block[2] >> 4)];
     encoded[2] = digit_set[((block[2] & 0x0F) << 6) | (block[3] >> 2)];
     encoded[3] = digit_set[((block[3] & 0x03) << 8) | (block[4])];
-    
+
     if (len == 4)
     {
         encoded[3] = digit_set[1024 + ((block[3] & 0x03) >> 8)];
@@ -150,7 +150,7 @@ const unsigned char *BaseHangulEncodeBlock(uint16_t *encoded, const unsigned cha
             encoded[i] = digit_set[1028];
         }
     }
-    
+
     return input + min(len, 5);
 }
 
@@ -173,11 +173,11 @@ uint16_t CharcterToIndex(uint16_t ch)
             {
                 return (i - 1024) << 8;
             }
-            
+
             return i;
         }
     }
-    
+
     assert(!"Invalid character for basehangul");
 }
 
@@ -187,34 +187,34 @@ const uint16_t *BaseHangulDecodeBlock(unsigned char *decoded, const uint16_t *en
     {
         return encoded;
     }
-    
+
     uint16_t block[4] = {0};
     for (int i = 0, valid_block_size = min(len, 4); i < valid_block_size; ++i)
     {
         block[i] = CharcterToIndex(*encoded);
         ++encoded;
     }
-    
+
     decoded[0] = block[0] >> 2;
     decoded[1] = ((block[0] & 0x0003) << 6) | (block[1] >> 4);
     if (len < 1 || block[1] == 0)
     {
         return encoded;
     }
-    
+
     decoded[2] = ((block[1] & 0x000F) << 4) | (block[2] >> 6);
     if (len < 2 || block[2] == 0)
     {
         return encoded;
     }
-    
+
     decoded[3] = ((block[2] & 0x003F) << 2) | (block[3] >> 8);
     if (len < 3 || block[3] == 0)
     {
         return encoded;
     }
-    
+
     decoded[4] = (block[3] & 0x00FF);
-    
+
     return encoded;
 }
