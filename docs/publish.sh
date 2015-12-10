34288fdbdfd33cd8eb61b2c57f6b29de13676d3d
Gyusun Yeom <omniavinco@gmail.com>
2015-12-11 02:08:26 +0900

    Generate docs and push it

---
+++ docs/publish.sh
@@ -0,0 +1,24 @@
+prepare() {
+    git config --global user.email "omniavinco@gmail.com"
+    git config --global user.name "travis-ci"
+    git config push.default simple
+    mkdir -p .travis
+    openssl aes-256-cbc -K $encrypted_4b7a7054081f_key -iv $encrypted_4b7a7054081f_iv -in id_rsa.enc -out id_rsa -d
+    chmod 600 .travis/id_rsa
+    eval `ssh-agent -s`
+    ssh-add .travis/id_rsa
+    virtualenv .venv
+    . ./.venv/bin/activate
+    pip install ghp-import
+}
+
+publish() {
+    pushd docs
+    doxygen doxygen.conf
+    git remote remove origin
+    git remote add origin git@github.com:Perlmint/libbasehangul.git
+    git fetch origin
+    ghp-import out/html
+    git push origin gh-pages
+    popd
+}
